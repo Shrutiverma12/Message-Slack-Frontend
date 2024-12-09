9a771814a00bd85f1189787e30d6bd531afec56b
Shruti Verma <shrutive1009@gmail.com>
2024-12-09 21:30:31 +0530

    Added protected route

--- src/hooks/apis/auth/useSignin.js
+++ src/hooks/apis/auth/useSignin.js
@@ -1,10 +1,12 @@
 import { useMutation } from '@tanstack/react-query';
 
 import { signInRequest } from '@/apis/auth';
+import { useAuth } from '@/hooks/context/useAuth';
 import { useToast } from '@/hooks/use-toast';
 
 export const useSignin = () => {
   const { toast } = useToast();
+  const { setAuth } = useAuth();
   const {
     isPending,
     isSuccess,
@@ -17,6 +19,12 @@ export const useSignin = () => {
       const userObject = JSON.stringify(response.data);
       localStorage.setItem('user', userObject);
       localStorage.setItem('token', response.data.token);
+      setAuth({
+        token: response.data.token,
+        user: response.data,
+        loading: false,
+      });
+
       toast({
         title: 'Successfully signed in',
         message: 'You will be redirect to the login page in a few second',
